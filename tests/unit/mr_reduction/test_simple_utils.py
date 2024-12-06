b4b2e4025da790c232e27225e3d7ab9998b888fc
Jose Borreguero <borreguero@gmail.com>
2024-12-06 16:11:51 -0500

    new class SampleLogs to facilitate probing the logs
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

---
+++ tests/unit/mr_reduction/test_simple_utils.py
@@ -0,0 +1,63 @@
+# standard imports
+from unittest.mock import MagicMock
+
+import pytest
+
+# third party imports
+from mantid.simpleapi import AddSampleLog, CreateWorkspace, DeleteWorkspace, mtd
+
+# mr_reduction imports
+from mr_reduction.simple_utils import SampleLogs
+
+
+class TestSampleLogs:
+    @classmethod
+    def setup_class(cls):
+        """
+        Setup method runs once for the whole test suite.
+
+        This method creates a workspace with sample data and adds several sample logs
+        of different types (integer, float, string, and number series) to the workspace.
+        """
+        ws = CreateWorkspace(DataX=[0, 1], DataY=[0, 10], OutputWorkspace=mtd.unique_hidden_name())
+        AddSampleLog(ws, LogName="property_int", LogText="42", LogType="Number", NumberType="Int")
+        AddSampleLog(ws, LogName="property_float", LogText="3.14", LogType="Number", NumberType="Double")
+        AddSampleLog(ws, LogName="property_str", LogText="hello", LogType="String")
+        AddSampleLog(ws, LogName="property_series", LogText="42", LogType="Number Series")
+        cls.workspace = str(ws)
+
+    @classmethod
+    def teardown_class(cls):
+        """Delete the workspace after all tests in the suite have run."""
+        DeleteWorkspace(cls.workspace)
+
+    def test_initialization(self):
+        return SampleLogs(self.workspace)
+
+    def test_contains_property(self):
+        sample_logs = SampleLogs(self.workspace)
+        for property_name in ["property_int", "property_float", "property_str", "property_series"]:
+            assert property_name in sample_logs
+        assert sample_logs["property_int"] == 42
+        assert sample_logs["property_float"] == 3.14
+        assert sample_logs["property_str"] == "hello"
+        assert sample_logs["property_series"] == 42
+
+    def test_does_not_contain_property(self):
+        sample_logs = SampleLogs(self.workspace)
+        assert "property_unknown" not in sample_logs
+
+    def test_property(self):
+        sample_logs = SampleLogs(self.workspace)
+        assert sample_logs.property("property_int").value == 42
+        assert sample_logs.property("property_float").value == 3.14
+        assert sample_logs.property("property_str").value == "hello"
+        assert sample_logs.property("property_series").firstValue() == 42
+
+    def test_mean(self):
+        sample_logs = SampleLogs(self.workspace)
+        assert sample_logs.mean("property_series") == 42
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
