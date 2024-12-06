40bcea48a96e4cd117479121b7622f089025740d
Jose Borreguero <borreguero@gmail.com>
2024-12-05 20:06:49 -0500

    Add custom type hints for Mantid workspaces
    Introduce `CrossSectionEventWorkspaces` and `MantidWorkspace` type aliases to improve type safety and clarity in code involving Mantid operations. These custom types help ensure consistent usage of Mantid workspace objects and facilitate easier maintenance and readability.
    
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

---
+++ src/mr_reduction/types.py
@@ -0,0 +1,19 @@
+"""
+Compendium of custom type hints.
+
+"""
+
+# standard library imports
+from typing import List, TypeAlias, Union
+
+# third party imports
+import mantid
+
+"""
+List of EvenWorkspace objects representing different cross-sections (Off_Off, Off_on, ...),
+usually the result of running mantid algorithm MRFilterCrossSections on an input Nexus events file.
+"""
+CrossSectionEventWorkspaces: TypeAlias = List[mantid.dataobjects.EventWorkspace]
+
+"""Any type of Mantid workspace"""
+MantidWorkspace: TypeAlias = Union[str, mantid.api.Workspace]
