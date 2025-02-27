f4b9c1c2b554ecfa6f519b8f4c28dae6074a20ea
Jose Borreguero <borreguero@gmail.com>
2025-02-27 11:25:41 -0500

    new function collect_search_directories
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- src/mr_reduction/settings.py
+++ src/mr_reduction/settings.py
@@ -1,3 +1,6 @@
+import os
+from typing import List, Union
+
 """
 Reduction settings for MR
 """
@@ -22,7 +25,24 @@ TOF_BIN = 400.0
 
 
 def ar_out_dir(ipts: str) -> str:
+    assert "IPTS-" in ipts
     return "/SNS/REF_M/%(ipts)s/shared/autoreduce/" % {"ipts": ipts}
+
+
+def collect_search_directories(ipts: str, extra: Union[str, List[str]] = None) -> list[str]:
+    """
+    Collect a list of directories to search for autoreduced data.
+    """
+    search_dirs = list()  # collect the directories in this list
+    if isinstance(extra, str):
+        extra = [extra]
+    if extra:
+        for extra_dir in extra:
+            if extra_dir is not None and extra_dir not in search_dirs and os.path.isdir(extra_dir):
+                search_dirs.append(extra_dir)
+    if ar_out_dir(ipts) not in search_dirs:  # prevent unintentional duplicates
+        search_dirs.append(ar_out_dir(ipts))
+    return search_dirs
 
 
 def nexus_data_dir(ipts: str) -> str:
