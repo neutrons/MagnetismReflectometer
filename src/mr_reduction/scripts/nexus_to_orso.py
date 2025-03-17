c9bac8cd7469c4807a4c6191843a3f7a10873d5b
Jose Borreguero <borreguero@gmail.com>
2025-03-17 17:02:42 -0400

    enable more than one input Nexus file to be passed to nexus_to_orso
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- src/mr_reduction/scripts/nexus_to_orso.py
+++ src/mr_reduction/scripts/nexus_to_orso.py
@@ -3,6 +3,7 @@
 
 # standard imports
 import argparse
+import os
 
 # third party imports
 import argcomplete
@@ -13,17 +14,26 @@ from mr_reduction.io_orso import write_orso
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Convert a Nexus file resulting from autoreduction to *.ort ORSO")
-    parser.add_argument("--nexus", required=True, help="Path to the input Nexus file")
+    parser = argparse.ArgumentParser(
+        description="Convert one or more Nexus files resulting from autoreduction to a *.ort ORSO files"
+    )
+    parser.add_argument("--nexus", required=True, help="Path to the input Nexus file(s)")
     parser.add_argument("--ort", required=True, help="Path to the output *.ort ORSO file")
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
 
-    # Load the Nexus file
-    reflectivity_workspace = LoadNexusProcessed(args.nexus)
+    # Load the Nexus file(s)
+    workspace_list = []
+    valid_suffix = "_autoreduce.nxs.h5"
+    for nexus_path in args.nexus.split():
+        if nexus_path.endswith(valid_suffix) is False:
+            raise ValueError(f"Input file {nexus_path} is not a valid Nexus autoreduced file")
+        workspace_name = os.path.basename(nexus_path).removesuffix(valid_suffix)
+        LoadNexusProcessed(Filename=nexus_path, OutputWorkspace=workspace_name)
+        workspace_list.append(workspace_name)
 
     # Write the ORSO file
-    write_orso([reflectivity_workspace], args.ort)
+    write_orso(workspace_list, args.ort)
 
 
 if __name__ == "__main__":
