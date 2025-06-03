a5524226a8dd603bcc6e2a769660ac7f44f9ee69
Jose Borreguero <borreguero@gmail.com>
2025-06-03 15:42:19 -0400

    Increase Gunicorn timeout to 600 seconds in reduce_REF_M_run.sh
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- src/mr_autoreduce/reduce_REF_M_run.sh
+++ src/mr_autoreduce/reduce_REF_M_run.sh
@@ -3,4 +3,4 @@ echo "* POINT YOUR BROWSER TO http://127.0.0.1:5000/"
 echo "**********************************************"
 
 export PYTHONPATH="${PYTHONPATH}:$(dirname "$0")"
-exec gunicorn --timeout 300 --bind :5000 mr_autoreduce.reduce_REF_M_run:app
+exec gunicorn --timeout 600 --bind :5000 mr_autoreduce.reduce_REF_M_run:app
